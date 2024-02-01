00bf9ecfb51216253a0856bc4438b0e4300bbdf9
Anders Sjöberg <anders.sjoberg@sileon.com>
2024-02-01 12:10:36 +0100

    Random grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,61 @@
 <script>
+	// @ts-nocheck
+
 	import { getPossibleMoves, findAll, visited } from '$lib/calculations.js';
+	import { onMount } from 'svelte';
 
-	var theGrid = [
-		[0, 0, 2, 1, 3],
-		[0, 1, 3, 0, 2],
-		[2, 1, 1, 0, 1],
-		[0, 2, 0, 0, 0],
-		[0, 2, 3, 0, 2]
-	];
+	var theGrid = undefined;
+	var rows = undefined;
+	var cols = undefined;
+	var startX = 0;
+	var startY = 0;
+  var pm = undefined;
 
-	var rows = Array.from({ length: theGrid.length }, (_, i) => i);
-	var cols = Array.from({ length: theGrid[0].length }, (_, i) => i);
-	var startX = 1;
-	var startY = 3;
-	var pm = getPossibleMoves(theGrid, startX, startY);
-	// @ts-ignore
+	function randomiseGrid() {
+		const gridSize = Math.floor(Math.random() * 17) + 4;
+		theGrid = Array.from({ length: gridSize }, () =>
+			Array.from({ length: gridSize }, () => Math.floor(Math.random() * 11))
+		);
+		rows = Array.from({ length: theGrid.length }, (_, i) => i);
+		cols = Array.from({ length: theGrid[0].length }, (_, i) => i);
+		startX = Math.floor(Math.random() * gridSize);
+		startY = Math.floor(Math.random() * gridSize);
+		var pm = getPossibleMoves(theGrid, startX, startY);
+
+		findAll(theGrid, startX, startY);
+	}
+
 	function checkMove(col, row) {
-		return pm.some((cell) => cell.x === col && cell.y === row);
+		return pm?.some((cell) => cell.x === col && cell.y === row);
 	}
 
-	// @ts-ignore
 	function checkVisited(col, row) {
-		return visited.some((cell) => cell.x === col && cell.y === row);
+		return visited?.some((cell) => cell.x === col && cell.y === row);
 	}
 
-	var f = findAll(theGrid, startX, startY);
+	onMount(() => {
+		randomiseGrid();
+	});
 </script>
 
 <div class="grid">
-	{#each rows as row}
-		{#each cols as col}
-			<div
-				style="grid-row: {row + 1}; grid-column: {col + 1};"
-				class="cell"
-				class:water={checkMove(col, row)}
-				class:start={startX === col && startY === row}
-				class:visited={checkVisited(col, row)}
-			>
-				<div>
-					{theGrid[row][col]}
+	{#if rows && cols && theGrid}
+		{#each rows as row}
+			{#each cols as col}
+				<div
+					style="grid-row: {row + 1}; grid-column: {col + 1};"
+					class="cell"
+					class:water={checkMove(col, row)}
+					class:start={startX === col && startY === row}
+					class:visited={checkVisited(col, row)}
+				>
+					<div>
+						{theGrid[row][col]}
+					</div>
 				</div>
-			</div>
+			{/each}
 		{/each}
-	{/each}
+	{/if}
 </div>
 
 <style>
@@ -59,11 +72,11 @@
 	}
 
 	.cell {
-    display: grid;
-    border: 1px solid black;
-    font-size: 2em;
-    justify-content: center;
-    align-items: center;
+		display: grid;
+		border: 1px solid black;
+		font-size: 2em;
+		justify-content: center;
+		align-items: center;
 	}
 	.water {
 		background-color: rgb(142, 142, 255);
